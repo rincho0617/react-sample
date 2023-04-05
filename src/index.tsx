18b4bf70a2424c2ef3f22f14fd1d01d780f8dc75
rincho0617 <komorin0617kazuki@gmail.com>
2023-04-05 21:00:32 +0900

    update

--- src/index.tsx
+++ src/index.tsx
@@ -10,6 +10,9 @@ import Counter from './components/UseStateSample';
 import { Parent } from './components/ButtonSample';
 import { UseMemoSample } from './components/UseMemoSample';
 import { Clock } from './components/Clock';
+import { ImageUploader } from './components/UseRefSample';
+import { Input } from './components/UseInput';
+
 
 const root = ReactDOM.createRoot(
   // index.htmlにあるrootをIDに持つ要素を指定している
@@ -19,7 +22,7 @@ root.render(
   // <h1>見出し</h1>
   // 描画するJSXタグを指定している
   <React.StrictMode>
-    <Clock/>
+    <Input/>
   </React.StrictMode>
 );
 
